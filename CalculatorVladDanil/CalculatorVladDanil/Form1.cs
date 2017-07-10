44c7f16e8fd0c737589b818f0bb9ff8853082827
Dany Zhukov <danil2010zhukov55@gmail.com>
2017-07-10 17:09:02 +0600

    Final version with factory (calculator)
    Using factories to make 4 calculators except one

--- CalculatorVladDanil/CalculatorVladDanil/Form1.cs
+++ CalculatorVladDanil/CalculatorVladDanil/Form1.cs
@@ -12,27 +12,10 @@ namespace CalculatorVladDanil
 
         public void Clicking(object sender, EventArgs e)
         {
-            double result;
             var firstArgument = Convert.ToDouble(textBoxEntering1.Text);
             var secondArgument = Convert.ToDouble(textBoxEntering2.Text);
-            switch (((Button)sender).Name)
-            {
-                case "buttonPlus":
-                    result = firstArgument + secondArgument;
-                    break;
-                case "buttonMinus":
-                    result = firstArgument - secondArgument;
-                    break;
-                case "buttonMulti":
-                    result = firstArgument * secondArgument;
-                    break;
-                case "buttonDivision":
-                    result = firstArgument / secondArgument;
-                    break;
-                default:
-                    throw new Exception("Неизвестная операция");
-            }
-
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator(((Button)sender).Name);
+            double result = calculator.ExecuteOperation(firstArgument, secondArgument);
             labelOut.Text = result.ToString();
         }
     }
